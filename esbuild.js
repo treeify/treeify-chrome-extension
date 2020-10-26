89224b5ddc0eb0138314ea9205633330a1ab9608
ootidea <ootidea@gmail.com>
2020-10-26 09:03:12 +0900

    空のエントリーポイント群を追加

--- esbuild.js
+++ esbuild.js
@@ -1,7 +1,12 @@
 const {build} = require('esbuild')
 
 build({
-  entryPoints: [],
+  entryPoints: [
+    './src/Background/entryPoint.ts',
+    './src/BrowserAction/entryPoint.ts',
+    './src/ContentScript/entryPoint.ts',
+    './src/TreeifyWindow/entryPoint.ts',
+  ],
   outdir: './dist',
   bundle: true,
   sourcemap: 'inline',
