528881b1e27a3176497be6831ace9891abb5b35c
ootidea <ootidea@gmail.com>
2020-10-28 21:13:40 +0900

    assert系関数を追加

---
+++ src/Common/Debug/assert.ts
@@ -0,0 +1,33 @@
+import {StackTrace} from 'src/Common/Debug/StackTrace'
+
+/**
+ * nullではないことを表明する。
+ * もしnullの場合は例外を投げる。
+ */
+export function assertNonNull<T>(value: T | null): asserts value is T {
+  if (value === null) {
+    const argString = new StackTrace().getStackFrameAt(1).getArgString()
+    throw new Error(`アサーションエラー: assertNonNull(${argString})`)
+  }
+}
+
+/**
+ * undefinedではないことを表明する。
+ * もしundefinedの場合は例外を投げる。
+ */
+export function assertNonUndefined<T>(value: T | undefined): asserts value is T {
+  if (value === undefined) {
+    const argString = new StackTrace().getStackFrameAt(1).getArgString()
+    throw new Error(`アサーションエラー: assertNonUndefined(${argString})`)
+  }
+}
+
+/**
+ * 与えられた式がnever型であることを表明する。
+ * 他のassert系関数と異なり、静的に検査される。
+ * 実行されると引数の値によらず例外を投げる。
+ */
+export function assertNeverType(value: never): never {
+  const argString = new StackTrace().getStackFrameAt(1).getArgString()
+  throw new Error(`アサーションエラー: assertNeverType(${argString})`)
+}
