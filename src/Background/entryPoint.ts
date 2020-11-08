8ccdee6223dc12c2d0fdf15a6b8c6a9e61704b42
ootidea <ootidea@gmail.com>
2020-11-08 16:26:11 +0900

    Treeifyウィンドウを開くChrome拡張機能コマンドを定義し、Ctrl + Bに割り当て

--- src/Background/entryPoint.ts
+++ src/Background/entryPoint.ts
@@ -2,3 +2,13 @@ import {TreeifyWindow} from 'src/TreeifyWindow/TreeifyWindow'
 
 // Treeifyウィンドウを開く
 TreeifyWindow.open()
+
+// chrome://extensions/shortcuts で設定されるコマンド呼び出しのリスナー
+chrome.commands.onCommand.addListener((commandName) => {
+  switch (commandName) {
+    case 'open-treeify-window':
+      // Treeifyウィンドウを開く
+      TreeifyWindow.open()
+      break
+  }
+})
