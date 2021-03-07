ecb56452fc5076d6d46c34de82342063ecf77e23
ootidea <ootidea@gmail.com>
2021-03-07 14:17:51 +0900

    Macの修飾キーとWindowsの修飾キーの違いを吸収する簡単な仕組みを導入

--- src/TreeifyWindow/Model/InputId.ts
+++ src/TreeifyWindow/Model/InputId.ts
@@ -1,13 +1,20 @@
+import UAParser from 'ua-parser-js'
+
 /**
  * キーボードまたはマウスからの入力の種類を一意に表す型。
  * 例えばShiftキーを押しながらSpaceキーを押した場合、"0100Space"となる。
  *
- * TODO: Macの修飾キーに対応する
  * 4桁の数字は、4種類の修飾キーがそれぞれ押されている(1)か押されていない(0)かを表す。
- * 1桁目：Ctrl
- * 2桁目：Shift
- * 3桁目：Alt
- * 4桁目：Windows
+ * Windowsの場合
+ *  1桁目：Ctrl
+ *  2桁目：Shift
+ *  3桁目：Alt
+ *  4桁目：Windows
+ * Macの場合
+ *  1桁目：command
+ *  2桁目：shift
+ *  3桁目：option
+ *  4桁目：control
  *
  * 5文字目以降の部分は、キーボード入力の場合はevent.keyの値、マウス入力の場合は`MouseButton${event.button}`となる。
  */
@@ -23,11 +30,20 @@ export namespace InputId {
   }
 
   function getModifierKeysId(event: KeyboardEvent | MouseEvent): string {
-    return [
-      event.ctrlKey ? '1' : '0',
-      event.shiftKey ? '1' : '0',
-      event.altKey ? '1' : '0',
-      event.metaKey ? '1' : '0',
-    ].join('')
+    if (new UAParser().getOS().name !== 'Mac OS') {
+      return [
+        event.ctrlKey ? '1' : '0',
+        event.shiftKey ? '1' : '0',
+        event.altKey ? '1' : '0',
+        event.metaKey ? '1' : '0',
+      ].join('')
+    } else {
+      return [
+        event.metaKey ? '1' : '0',
+        event.shiftKey ? '1' : '0',
+        event.altKey ? '1' : '0',
+        event.ctrlKey ? '1' : '0',
+      ].join('')
+    }
   }
 }
