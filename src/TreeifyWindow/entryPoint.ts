b9d4cdc67724dadd46cbc1a85473424afadc9e42
ootidea <ootidea@gmail.com>
2021-03-10 11:23:44 +0900

    TreeifyWindowのエントリーポイントの処理をasync関数化（awaitを使う準備）

--- src/TreeifyWindow/entryPoint.ts
+++ src/TreeifyWindow/entryPoint.ts
@@ -12,46 +12,50 @@ import {
 import {ItemTreeContentView} from 'src/TreeifyWindow/View/ItemTree/ItemTreeContentView'
 import {createRootViewModel, RootView} from 'src/TreeifyWindow/View/RootView'
 
-const spaRoot = document.getElementById('spa-root')!
-render(RootView(createRootViewModel(Model.instance.currentState)), spaRoot)
+entryPoint()
 
-Model.instance.addStateChangeListener((newState) => {
-  // render関数を呼ぶとfocusoutイベントが発生し、focusedItemPathがnullになるケースがある。
-  // なのでrender関数を呼ぶ前に取得しておく。
-  const focusedItemPath = newState.pages[newState.activePageId].focusedItemPath
+async function entryPoint() {
+  const spaRoot = document.getElementById('spa-root')!
+  render(RootView(createRootViewModel(Model.instance.currentState)), spaRoot)
 
-  render(RootView(createRootViewModel(newState)), spaRoot)
+  Model.instance.addStateChangeListener((newState) => {
+    // render関数を呼ぶとfocusoutイベントが発生し、focusedItemPathがnullになるケースがある。
+    // なのでrender関数を呼ぶ前に取得しておく。
+    const focusedItemPath = newState.pages[newState.activePageId].focusedItemPath
 
-  if (focusedItemPath !== null) {
-    const id = ItemTreeContentView.focusableDomElementId(focusedItemPath)
-    const focusableElement = document.getElementById(id)
-    if (focusableElement !== null) {
-      // フォーカスアイテムが画面内に入るようスクロールする。
-      // blockに'center'を指定してもなぜか中央化してくれない（原因不明）。
-      focusableElement.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'nearest'})
+    render(RootView(createRootViewModel(newState)), spaRoot)
 
-      if (newState.itemTreeTextItemSelection !== null) {
-        // キャレット位置をModelからViewに反映する
-        setDomSelection(focusableElement, newState.itemTreeTextItemSelection)
-      } else {
-        // フォーカスアイテムをModelからViewに反映する
-        focusableElement.focus()
+    if (focusedItemPath !== null) {
+      const id = ItemTreeContentView.focusableDomElementId(focusedItemPath)
+      const focusableElement = document.getElementById(id)
+      if (focusableElement !== null) {
+        // フォーカスアイテムが画面内に入るようスクロールする。
+        // blockに'center'を指定してもなぜか中央化してくれない（原因不明）。
+        focusableElement.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'nearest'})
+
+        if (newState.itemTreeTextItemSelection !== null) {
+          // キャレット位置をModelからViewに反映する
+          setDomSelection(focusableElement, newState.itemTreeTextItemSelection)
+        } else {
+          // フォーカスアイテムをModelからViewに反映する
+          focusableElement.focus()
+        }
       }
     }
-  }
-})
+  })
 
-// バックグラウンドページなどからのメッセージを受信する
-chrome.runtime.onMessage.addListener(onMessage)
+  // バックグラウンドページなどからのメッセージを受信する
+  chrome.runtime.onMessage.addListener(onMessage)
 
-// タブイベントの監視を開始
-chrome.tabs.onCreated.addListener(onCreated)
-chrome.tabs.onUpdated.addListener(onUpdated)
-chrome.tabs.onRemoved.addListener(onRemoved)
-chrome.tabs.onActivated.addListener(onActivated)
+  // タブイベントの監視を開始
+  chrome.tabs.onCreated.addListener(onCreated)
+  chrome.tabs.onUpdated.addListener(onUpdated)
+  chrome.tabs.onRemoved.addListener(onRemoved)
+  chrome.tabs.onActivated.addListener(onActivated)
 
-// テキストアイテム内のキャレット位置の監視用
-document.addEventListener('selectionchange', (event) => {
-  NextState.setItemTreeTextItemSelection(getTextItemSelectionFromDom() ?? null)
-  NextState.commitSilently()
-})
+  // テキストアイテム内のキャレット位置の監視用
+  document.addEventListener('selectionchange', (event) => {
+    NextState.setItemTreeTextItemSelection(getTextItemSelectionFromDom() ?? null)
+    NextState.commitSilently()
+  })
+}
