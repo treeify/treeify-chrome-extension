3d2216b2324d550a3198fb1a750e7ada9ece0621
ootidea <ootidea@gmail.com>
2021-03-11 18:23:28 +0900

    DOM描画のコードをentryPointからExternalに移動

--- src/TreeifyWindow/External/External.ts
+++ src/TreeifyWindow/External/External.ts
@@ -1,6 +1,11 @@
 import {integer, ItemId, TabId} from 'src/Common/basicType'
 import {assertNonUndefined} from 'src/Common/Debug/assert'
 import {List} from 'immutable'
+import {render as renderWithLitHtml} from 'lit-html'
+import {createRootViewModel, RootView} from 'src/TreeifyWindow/View/RootView'
+import {ItemTreeContentView} from 'src/TreeifyWindow/View/ItemTree/ItemTreeContentView'
+import {setDomSelection} from 'src/TreeifyWindow/External/domTextSelection'
+import {State} from 'src/TreeifyWindow/Internal/State'
 import Tab = chrome.tabs.Tab
 
 /** TODO: コメント */
@@ -37,4 +42,45 @@ export namespace External {
    * chrome.tabs.onRemovedイベント時に、タブがアンロード由来で閉じられたのかを判定するために用いる。
    */
   export const hardUnloadedTabIds = new Set<integer>()
+
+  /** DOMの初回描画を行う */
+  export function render(state: State) {
+    const spaRoot = document.getElementById('spa-root')!
+    renderWithLitHtml(RootView(createRootViewModel(state)), spaRoot)
+  }
+
+  /** DOMを再描画する */
+  export function rerender(newState: State) {
+    const spaRoot = document.getElementById('spa-root')!
+
+    // render関数を呼ぶとfocusoutイベントが発生し、focusedItemPathがnullになるケースがある。
+    // なのでrender関数を呼ぶ前に取得しておく。
+    const focusedItemPath = newState.pages[newState.activePageId].focusedItemPath
+
+    renderWithLitHtml(RootView(createRootViewModel(newState)), spaRoot)
+
+    if (focusedItemPath !== null) {
+      const id = ItemTreeContentView.focusableDomElementId(focusedItemPath)
+      const focusableElement = document.getElementById(id)
+      if (focusableElement !== null) {
+        // フォーカスアイテムが画面内に入るようスクロールする。
+        // blockに'center'を指定してもなぜか中央化してくれない（原因不明）。
+        focusableElement.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'nearest'})
+
+        if (newState.itemTreeTextItemSelection !== null) {
+          // キャレット位置をModelからViewに反映する
+          setDomSelection(focusableElement, newState.itemTreeTextItemSelection)
+        } else {
+          // フォーカスアイテムをModelからViewに反映する
+          focusableElement.focus()
+        }
+      }
+    }
+
+    // データベースファイル書き出し
+    External.databaseFileHandle?.createWritable()?.then((stream) => {
+      stream.write(State.toJsonString(newState))
+      stream.close()
+    })
+  }
 }
--- src/TreeifyWindow/entryPoint.ts
+++ src/TreeifyWindow/entryPoint.ts
@@ -1,12 +1,6 @@
-import {render} from 'lit-html'
-import {
-  getTextItemSelectionFromDom,
-  setDomSelection,
-} from 'src/TreeifyWindow/External/domTextSelection'
+import {getTextItemSelectionFromDom} from 'src/TreeifyWindow/External/domTextSelection'
 import {Internal} from 'src/TreeifyWindow/Internal/Internal'
 import {NextState} from 'src/TreeifyWindow/Internal/NextState'
-import {ItemTreeContentView} from 'src/TreeifyWindow/View/ItemTree/ItemTreeContentView'
-import {createRootViewModel, RootView} from 'src/TreeifyWindow/View/RootView'
 import {
   matchTabsAndWebPageItems,
   onActivated,
@@ -15,7 +9,6 @@ import {
   onRemoved,
   onUpdated,
 } from 'src/TreeifyWindow/External/chromeEventListeners'
-import {State} from 'src/TreeifyWindow/Internal/State'
 import {External} from 'src/TreeifyWindow/External/External'
 
 entryPoint()
@@ -24,39 +17,10 @@ async function entryPoint() {
   // Treeifyウィンドウ起動時点で既に存在するタブをウェブページアイテムと紐付ける
   await matchTabsAndWebPageItems()
 
-  const spaRoot = document.getElementById('spa-root')!
-  render(RootView(createRootViewModel(Internal.currentState)), spaRoot)
+  External.render(Internal.currentState)
 
   Internal.addStateChangeListener((newState) => {
-    // render関数を呼ぶとfocusoutイベントが発生し、focusedItemPathがnullになるケースがある。
-    // なのでrender関数を呼ぶ前に取得しておく。
-    const focusedItemPath = newState.pages[newState.activePageId].focusedItemPath
-
-    render(RootView(createRootViewModel(newState)), spaRoot)
-
-    if (focusedItemPath !== null) {
-      const id = ItemTreeContentView.focusableDomElementId(focusedItemPath)
-      const focusableElement = document.getElementById(id)
-      if (focusableElement !== null) {
-        // フォーカスアイテムが画面内に入るようスクロールする。
-        // blockに'center'を指定してもなぜか中央化してくれない（原因不明）。
-        focusableElement.scrollIntoView({behavior: 'smooth', block: 'nearest', inline: 'nearest'})
-
-        if (newState.itemTreeTextItemSelection !== null) {
-          // キャレット位置をModelからViewに反映する
-          setDomSelection(focusableElement, newState.itemTreeTextItemSelection)
-        } else {
-          // フォーカスアイテムをModelからViewに反映する
-          focusableElement.focus()
-        }
-      }
-    }
-
-    // データベースファイル書き出し
-    External.databaseFileHandle?.createWritable()?.then((value) => {
-      value.write(State.toJsonString(newState))
-      value.close()
-    })
+    External.rerender(newState)
   })
 
   // バックグラウンドページなどからのメッセージを受信する
