e7ad726352d6b09d4b4d058f705c634848985689
ootidea <ootidea@gmail.com>
2021-03-17 17:43:35 +0900

    ウェブページアイテム以外に対してタイトル設定ダイアログが表示される不具合を修正

--- src/TreeifyWindow/Internal/NullaryCommand/dialog.ts
+++ src/TreeifyWindow/Internal/NullaryCommand/dialog.ts
@@ -1,7 +1,15 @@
 import {NextState} from 'src/TreeifyWindow/Internal/NextState'
+import {ItemType} from 'src/Common/basicType'
 
 /** アイテムの種類に応じた編集系ダイアログなどを出す */
 export function edit() {
-  // ウェブページアイテムのタイトル設定ダイアログを表示する
-  NextState.setWebPageItemTitleSettingDialog({})
+  const itemId = NextState.getTargetItemPath().itemId
+  switch (NextState.getItemType(itemId)) {
+    case ItemType.TEXT:
+      break
+    case ItemType.WEB_PAGE:
+      // ウェブページアイテムのタイトル設定ダイアログを表示する
+      NextState.setWebPageItemTitleSettingDialog({})
+      break
+  }
 }
