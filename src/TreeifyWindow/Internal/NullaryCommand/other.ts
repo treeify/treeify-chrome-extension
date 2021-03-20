e23c1091d0b646dc6372df9ddab76f64cd829947
ootidea <ootidea@gmail.com>
2021-03-20 13:08:33 +0900

    データフォルダ選択ダイアログでフォルダを選んだ際にデータを読み取ってStateに反映する処理を実装

--- src/TreeifyWindow/Internal/NullaryCommand/other.ts
+++ src/TreeifyWindow/Internal/NullaryCommand/other.ts
@@ -1,13 +1,18 @@
 import {doAsyncWithErrorHandling} from 'src/Common/Debug/report'
 import {External} from 'src/TreeifyWindow/External/External'
 import {DataFolder} from 'src/TreeifyWindow/Internal/NullaryCommand/DataFolder'
+import {Chunk} from 'src/TreeifyWindow/Internal/Chunk'
+import {cleanup, startup} from 'src/TreeifyWindow/startup'
+import {State} from 'src/TreeifyWindow/Internal/State'
 
 /** データフォルダ選択ダイアログを開く */
 export function openDataFolderPicker() {
   doAsyncWithErrorHandling(async () => {
     const dataFolder = new DataFolder(await showDirectoryPicker())
+    const state = Chunk.inflateStateFromChunks(await dataFolder.readAllChunks())
+    await cleanup()
+    // ↑のcleanup()によってExternal.instance.dataFolderはリセットされるので、このタイミングで設定する
     External.instance.dataFolder = dataFolder
-
-    // TODO: ファイル群を読み込んでパースし、cleanupとstartupを行う
+    await startup(state as State)
   })
 }
