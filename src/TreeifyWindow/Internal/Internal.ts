15e832515c2e05cdb1aabafa8f4a4e6472296f44
ootidea <ootidea@gmail.com>
2021-03-25 09:50:44 +0900

    Internal内の変数の定義位置を変更

--- src/TreeifyWindow/Internal/Internal.ts
+++ src/TreeifyWindow/Internal/Internal.ts
@@ -8,6 +8,13 @@ import {assertNonUndefined} from 'src/Common/Debug/assert'
 export class Internal {
   private static _instance: Internal | undefined
 
+  readonly state: State = Internal.createInitialState()
+  readonly mutatedPropertyPaths = new Set<PropertyPath>()
+
+  private readonly stateChangeListeners = new Set<
+    (newState: State, mutatedPropertyPaths: Set<PropertyPath>) => void
+  >()
+
   private constructor(initialState: State) {
     this.state = initialState
   }
@@ -34,12 +41,6 @@ export class Internal {
   static cleanup() {
     this._instance = undefined
   }
-
-  private readonly stateChangeListeners = new Set<
-    (newState: State, mutatedPropertyPaths: Set<PropertyPath>) => void
-  >()
-  readonly state: State = Internal.createInitialState()
-  readonly mutatedPropertyPaths = new Set<PropertyPath>()
 
   /** Stateへの変更を確定し、stateChangeListenerに通知する */
   commit() {
