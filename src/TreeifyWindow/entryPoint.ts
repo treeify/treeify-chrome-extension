953525989b69ae2ec92d0ad9f989e0721825bd76
ootidea <ootidea@gmail.com>
2021-04-01 19:39:43 +0900

    Treeifyウィンドウ起動時にデバイスIDをコンソール出力するよう修正

--- src/TreeifyWindow/entryPoint.ts
+++ src/TreeifyWindow/entryPoint.ts
@@ -1,7 +1,9 @@
 import {doWithErrorHandling} from 'src/Common/Debug/report'
+import {DeviceId} from 'src/TreeifyWindow/DeviceId'
 import {Internal} from 'src/TreeifyWindow/Internal/Internal'
 import {startup} from 'src/TreeifyWindow/startup'
 
 doWithErrorHandling(() => {
+  console.log('デバイスID = ' + DeviceId.get())
   startup(Internal.createInitialState())
 })
