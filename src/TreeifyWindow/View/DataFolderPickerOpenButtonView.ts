eef67227335141a1243d0deca0f4e23b7837bc4b
ootidea <ootidea@gmail.com>
2021-04-01 15:06:31 +0900

    データフォルダピッカーを開くボタンに擬似リップルエフェクトを実装

--- src/TreeifyWindow/View/DataFolderPickerOpenButtonView.ts
+++ src/TreeifyWindow/View/DataFolderPickerOpenButtonView.ts
@@ -4,8 +4,8 @@ import {NullaryCommand} from 'src/TreeifyWindow/Internal/NullaryCommand'
 
 export function DataFolderPickerOpenButtonView() {
   return html`
-    <div class="toolbar-icon-button">
-      <div class="data-folder-picker-open-button_icon" @click=${onClick}></div>
+    <div class="toolbar-icon-button" @click=${onClick}>
+      <div class="data-folder-picker-open-button_icon"></div>
     </div>
   `
 }
