cd2230debcb6e1ef95bbcb1d433c498cbdba4fe5
ootidea <ootidea@gmail.com>
2021-04-09 17:47:56 +0900

    データフォルダボタン押下時の処理の見落としを解消

--- src/TreeifyWindow/Internal/NullaryCommand/other.ts
+++ src/TreeifyWindow/Internal/NullaryCommand/other.ts
@@ -23,16 +23,25 @@ export async function saveToDataFolder() {
     if (unknownUpdatedDeviceId === undefined) {
       // もし自身の知らない他デバイスの更新がなければ
 
-      // Stateを読み込んで事実上の再起動を行う（それまでの変更は破棄する）
       const chunks = await External.instance.dataFolder.readAllChunks()
-      const state = Chunk.inflateStateFromChunks(chunks)
-      const dataFolder = External.instance.dataFolder
-      await cleanup()
-      // ↑のcleanup()によってExternal.instance.dataFolderはリセットされるので、このタイミングで設定する
-      External.instance.dataFolder = dataFolder
-      await startup(state as State)
+      if (chunks.isEmpty()) {
+        // 自デバイスフォルダが無い場合
+
+        // メモリ上のStateを自デバイスフォルダに書き込む
+        const allChunks = Chunk.createAllChunks(Internal.instance.state)
+        const filtered = allChunks.filter((chunks) => chunks !== undefined) as List<Chunk>
+        await External.instance.dataFolder.writeChunks(filtered)
+      } else {
+        // 自デバイスフォルダの内容からStateを読み込んで事実上の再起動を行う
+        const state = Chunk.inflateStateFromChunks(chunks)
+        const dataFolder = External.instance.dataFolder
+        await cleanup()
+        // ↑のcleanup()によってExternal.instance.dataFolderはリセットされるので、このタイミングで設定する
+        External.instance.dataFolder = dataFolder
+        await startup(state as State)
+      }
     } else {
-      // もし自身の知らない他デバイスの更新があれば（特に自デバイスフォルダが存在しなければ）
+      // もし自身の知らない他デバイスの更新があれば
       await External.instance.dataFolder.copyFrom(unknownUpdatedDeviceId)
 
       const chunks = await External.instance.dataFolder.readAllChunks()
@@ -69,6 +78,7 @@ export async function saveToDataFolder() {
       await startup(state as State)
     }
   }
+  CurrentState.commit()
 }
 
 /**
