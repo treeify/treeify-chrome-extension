e986492ad5f620cd0a2f088ceec0c649dcafde17
ootidea <ootidea@gmail.com>
2021-04-11 12:28:53 +0900

    Treeifyウィンドウ起動時にエラーを補足できない問題を解消

--- src/TreeifyWindow/entryPoint.ts
+++ src/TreeifyWindow/entryPoint.ts
@@ -1,9 +1,10 @@
-import {doWithErrorHandling} from 'src/Common/Debug/report'
+import {doAsyncWithErrorHandling} from 'src/Common/Debug/report'
 import {DeviceId} from 'src/TreeifyWindow/DeviceId'
 import {Internal} from 'src/TreeifyWindow/Internal/Internal'
 import {startup} from 'src/TreeifyWindow/startup'
 
-doWithErrorHandling(() => {
+doAsyncWithErrorHandling(async () => {
   console.log('デバイスID = ' + DeviceId.get())
-  startup(Internal.createInitialState())
+
+  await startup(Internal.createInitialState())
 })
