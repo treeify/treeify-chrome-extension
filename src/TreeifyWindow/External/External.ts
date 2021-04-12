de8aa74a4b32d0c95ce258e176e301206ab380d8
ootidea <ootidea@gmail.com>
2021-04-12 20:43:06 +0900

    コードブロックアイテムのシンタックスハイライトを行う仕組みを先行実装

--- src/TreeifyWindow/External/External.ts
+++ src/TreeifyWindow/External/External.ts
@@ -1,4 +1,5 @@
 import {createFocusTrap} from 'focus-trap'
+import hljs from 'highlight.js'
 import {List} from 'immutable'
 import {render as renderWithLitHtml} from 'lit-html'
 import md5 from 'md5'
@@ -85,6 +86,11 @@ export class External {
     const spaRoot = document.querySelector('.spa-root')
     assertNonNull(spaRoot)
     renderWithLitHtml(RootView(createRootViewModel(state)), spaRoot)
+
+    // シンタックスハイライトを行う
+    for (const codeBlockElement of document.querySelectorAll('pre code')) {
+      hljs.highlightBlock(codeBlockElement as HTMLElement)
+    }
 
     if (this.pendingFocusElementId !== undefined) {
       const focusableElement = document.getElementById(this.pendingFocusElementId)
