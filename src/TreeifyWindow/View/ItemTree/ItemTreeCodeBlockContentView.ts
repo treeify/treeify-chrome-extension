b6987df34b24e19eaf981c6bf5be615948970b7f
ootidea <ootidea@gmail.com>
2021-05-30 14:53:22 +0900

    コードブロックアイテムをクリックしても何も起こらない不具合を修正

--- src/TreeifyWindow/View/ItemTree/ItemTreeCodeBlockContentView.ts
+++ src/TreeifyWindow/View/ItemTree/ItemTreeCodeBlockContentView.ts
@@ -2,6 +2,7 @@ import hljs from 'highlight.js'
 import {List} from 'immutable'
 import {ItemType} from 'src/TreeifyWindow/basicType'
 import {doWithErrorCapture} from 'src/TreeifyWindow/errorCapture'
+import {External} from 'src/TreeifyWindow/External/External'
 import {CurrentState} from 'src/TreeifyWindow/Internal/CurrentState'
 import {ItemPath} from 'src/TreeifyWindow/Internal/ItemPath'
 import {State} from 'src/TreeifyWindow/Internal/State'
@@ -17,6 +18,7 @@ export type ItemTreeCodeBlockContentViewModel = {
   code: string
   language: string
   onFocus: (event: FocusEvent) => void
+  onClick: (event: Event) => void
 }
 
 export function createItemTreeCodeBlockContentViewModel(
@@ -38,8 +40,14 @@ export function createItemTreeCodeBlockContentViewModel(
         if (event.target instanceof Node) {
           getSelection()?.setPosition(event.target)
         }
-
+      })
+    },
+    onClick: (event) => {
+      doWithErrorCapture(() => {
         CurrentState.setTargetItemPath(itemPath)
+        External.instance.requestFocusAfterRendering(
+          ItemTreeContentView.focusableDomElementId(itemPath)
+        )
         CurrentState.commit()
       })
     },
@@ -51,7 +59,7 @@ export function ItemTreeCodeBlockContentView(viewModel: ItemTreeCodeBlockContent
   const id = ItemTreeContentView.focusableDomElementId(viewModel.itemPath)
   return createDivElement(
     {class: 'item-tree-code-block-content', id, tabindex: '0'},
-    {focus: viewModel.onFocus},
+    {focus: viewModel.onFocus, click: viewModel.onClick},
     [
       !viewModel.labels.isEmpty()
         ? createDivElement(
