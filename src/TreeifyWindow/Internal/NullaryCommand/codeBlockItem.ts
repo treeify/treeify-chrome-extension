47f8c871c12524afb59c2a6e88f7a68be4b8f3e1
ootidea <ootidea@gmail.com>
2021-06-02 20:12:16 +0900

    リファクタリング：アイテム作成コマンドでinsertBelowItem関数を活用

--- src/TreeifyWindow/Internal/NullaryCommand/codeBlockItem.ts
+++ src/TreeifyWindow/Internal/NullaryCommand/codeBlockItem.ts
@@ -1,25 +1,9 @@
-import {assertNonUndefined} from 'src/Common/Debug/assert'
 import {CurrentState} from 'src/TreeifyWindow/Internal/CurrentState'
-import {ItemPath} from 'src/TreeifyWindow/Internal/ItemPath'
 
 /** 空のコードブロックアイテムを作る */
 export function createEmptyCodeBlockItem() {
-  const targetItemPath = CurrentState.getTargetItemPath()
-
   const newItemId = CurrentState.createCodeBlockItem()
-
-  if (CurrentState.getDisplayingChildItemIds(targetItemPath).isEmpty()) {
-    CurrentState.insertNextSiblingItem(targetItemPath, newItemId)
-
-    // 作ったアイテムをフォーカスする
-    const newItemPath = ItemPath.createSiblingItemPath(targetItemPath, newItemId)
-    assertNonUndefined(newItemPath)
-    CurrentState.setTargetItemPath(newItemPath)
-  } else {
-    CurrentState.insertFirstChildItem(ItemPath.getItemId(targetItemPath), newItemId)
-
-    // 作ったアイテムをフォーカスする
-    const newItemPath = targetItemPath.push(newItemId)
-    CurrentState.setTargetItemPath(newItemPath)
-  }
+  const newItemPath = CurrentState.insertBelowItem(CurrentState.getTargetItemPath(), newItemId)
+  // 作ったアイテムをフォーカスする
+  CurrentState.setTargetItemPath(newItemPath)
 }
