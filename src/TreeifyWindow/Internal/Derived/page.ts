0f164fb6e4391a4dbc8c17dbfcb7cfc0b847588b
ootidea <ootidea@gmail.com>
2021-06-14 07:58:11 +0900

    Derived内の不要になっていた関数を削除

--- src/TreeifyWindow/Internal/Derived/page.ts
+++ src/TreeifyWindow/Internal/Derived/page.ts
@@ -2,20 +2,11 @@ import {ItemId} from 'src/TreeifyWindow/basicType'
 import {CurrentState} from 'src/TreeifyWindow/Internal/CurrentState'
 import {getContentAsPlainText} from 'src/TreeifyWindow/Internal/importAndExport'
 import {Internal} from 'src/TreeifyWindow/Internal/Internal'
-import {ItemPath} from 'src/TreeifyWindow/Internal/ItemPath'
 import {Readable} from 'svelte/store'
 
 /** 指定されたアイテムがページかどうかを返す */
 export function isPage(itemId: ItemId): Readable<boolean> {
   return Internal.d(() => Internal.instance.state.pages[itemId] !== undefined)
-}
-
-export function getTargetItemPath(): Readable<ItemPath> {
-  return Internal.d(() => CurrentState.getTargetItemPath())
-}
-
-export function getAnchorItemPath(): Readable<ItemPath> {
-  return Internal.d(() => CurrentState.getAnchorItemPath())
 }
 
 /** Treeifyウィンドウのタイトルとして表示する文字列を返す */
