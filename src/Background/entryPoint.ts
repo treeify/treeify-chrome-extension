5723b1d2ada5fcb913897a8ccdc9c8129925562e
ootidea <ootidea@gmail.com>
2021-06-19 10:31:45 +0900

    バックグラウンドスクリプトがTreeifyウィンドウを二重起動している不具合を修正

--- src/Background/entryPoint.ts
+++ src/Background/entryPoint.ts
@@ -1,15 +1,24 @@
 import CreateData = chrome.windows.CreateData
 
-const createData: CreateData = {
-  url: chrome.runtime.getURL('TreeifyWindow/index.html'),
-  type: 'popup',
-  // TODO: フルウィンドウモードで終了した場合は、次回起動時もフルウィンドウモードになってほしい気がする
-  state: 'normal',
-  width: 300,
-  // windowオブジェクトが使えないので下記のようには指定できない
-  // height: screen.availHeight,
-  top: 0,
-  left: 0,
-}
+;(async () => {
+  // Treeifyウィンドウがあればフォーカスする
+  const windows = await chrome.windows.getAll({populate: true, windowTypes: ['popup']})
+  const tabs = windows.flatMap((window) => window.tabs ?? [])
+  const treeifyWindowUrl = chrome.runtime.getURL('TreeifyWindow/index.html')
 
-chrome.windows.create(createData)
+  if (tabs.find((tab) => tab.url === treeifyWindowUrl) === undefined) {
+    const createData: CreateData = {
+      url: chrome.runtime.getURL('TreeifyWindow/index.html'),
+      type: 'popup',
+      // TODO: フルウィンドウモードで終了した場合は、次回起動時もフルウィンドウモードになってほしい気がする
+      state: 'normal',
+      width: 300,
+      // windowオブジェクトが使えないので下記のようには指定できない
+      // height: screen.availHeight,
+      top: 0,
+      left: 0,
+    }
+
+    await chrome.windows.create(createData)
+  }
+})()
