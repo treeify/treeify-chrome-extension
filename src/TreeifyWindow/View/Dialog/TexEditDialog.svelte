76946058aba765ff648a7bf18780df2cc4350378
ootidea <ootidea@gmail.com>
2021-06-23 16:03:24 +0900

    TeX編集ダイアログにリアルタイムプレビュー機能を追加
    想像してたよりずっと描画が速いのでいい感じになった。

--- src/TreeifyWindow/View/Dialog/TexEditDialog.svelte
+++ src/TreeifyWindow/View/Dialog/TexEditDialog.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import katex from 'katex'
   import {CurrentState} from '../../Internal/CurrentState'
   import {Rerenderer} from '../../Rerenderer'
   import CommonDialog from './CommonDialog.svelte'
@@ -6,17 +7,34 @@
 
   export let props: TexEditDialogProps
 
+  // リアルタイムプレビュー用の変数
+  let currentCode = props.code
+
   const onCloseDialog = () => {
     // ダイアログを閉じる
     CurrentState.setTexEditDialog(null)
     Rerenderer.instance.rerender()
   }
+
+  function onInput(event: Event) {
+    if (event.target instanceof HTMLElement) {
+      currentCode = event.target.textContent
+    }
+  }
 </script>
 
 <CommonDialog title="TeX編集" {onCloseDialog}>
   <div class="tex-edit-dialog_content">
-    <div class="tex-edit-dialog_code" contenteditable="plaintext-only" tabindex="0">
+    <div
+      class="tex-edit-dialog_code"
+      contenteditable="plaintext-only"
+      tabindex="0"
+      on:input={onInput}
+    >
       {props.code}
+    </div>
+    <div class="tex-edit-dialog_rendered-tex">
+      {@html katex.renderToString(currentCode, {throwOnError: false})}
     </div>
     <div class="tex-edit-dialog_button-area">
       <button on:click={props.onClickFinishButton}>完了</button>
@@ -34,6 +52,10 @@
     padding: 0.5em;
 
     outline: 1px solid hsl(0, 0%, 60%);
+  }
+
+  .tex-edit-dialog_rendered-tex {
+    margin-top: 1em;
   }
 
   .tex-edit-dialog_button-area {
