84b5e536e168289008199cdb33fb2cce8d32c3ae
ootidea <ootidea@gmail.com>
2021-06-28 16:53:26 +0900

    highlight.jsの初期化エラーが出なくなるよう修正。不要な言語定義ファイルを削除

--- src/TreeifyWindow/entryPoint.ts
+++ src/TreeifyWindow/entryPoint.ts
@@ -1,10 +1,13 @@
 import {DeviceId} from 'src/TreeifyWindow/DeviceId'
 import {doAsyncWithErrorCapture} from 'src/TreeifyWindow/errorCapture'
+import {registerLanguages} from 'src/TreeifyWindow/highlightJs'
 import {Internal} from 'src/TreeifyWindow/Internal/Internal'
 import {startup} from 'src/TreeifyWindow/startup'
 
 doAsyncWithErrorCapture(async () => {
   console.log('デバイスID = ' + DeviceId.get())
+
+  registerLanguages()
 
   await startup(Internal.createSampleState())
 })
