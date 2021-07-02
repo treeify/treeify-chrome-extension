8fa6befcd42bd8d6e6741f52afe252c326811192
ootidea <ootidea@gmail.com>
2021-07-02 16:13:25 +0900

    Ctrl+Enterキー押下時にTeX編集ダイアログを完了する機能を追加

--- src/TreeifyWindow/View/Dialog/TexEditDialogProps.ts
+++ src/TreeifyWindow/View/Dialog/TexEditDialogProps.ts
@@ -1,5 +1,6 @@
 import {assertNonNull} from 'src/Common/Debug/assert'
 import {CurrentState} from 'src/TreeifyWindow/Internal/CurrentState'
+import {InputId} from 'src/TreeifyWindow/Internal/InputId'
 import {Internal} from 'src/TreeifyWindow/Internal/Internal'
 import {ItemPath} from 'src/TreeifyWindow/Internal/ItemPath'
 import {TexEditDialog} from 'src/TreeifyWindow/Internal/State'
@@ -9,31 +10,41 @@ export type TexEditDialogProps = {
   code: string
   onClickFinishButton: () => void
   onClickCancelButton: () => void
+  onKeyDown: (event: KeyboardEvent) => void
 }
 
 export function createTexEditDialogProps(dialog: TexEditDialog): TexEditDialogProps {
   const targetItemPath = CurrentState.getTargetItemPath()
+  const onClickFinishButton = () => {
+    const targetItemId = ItemPath.getItemId(targetItemPath)
+
+    // コードを更新
+    const editBox = document.querySelector<HTMLDivElement>('.tex-edit-dialog_code')
+    assertNonNull(editBox)
+    CurrentState.setTexItemCode(targetItemId, editBox.textContent ?? '')
+
+    // タイムスタンプを更新
+    CurrentState.updateItemTimestamp(targetItemId)
+
+    // ダイアログを閉じる
+    CurrentState.setDialog(null)
+    Rerenderer.instance.rerender()
+  }
+
   return {
     code: Internal.instance.state.texItems[ItemPath.getItemId(targetItemPath)].code,
-    onClickFinishButton: () => {
-      const targetItemId = ItemPath.getItemId(targetItemPath)
-
-      // コードを更新
-      const editBox = document.querySelector<HTMLDivElement>('.tex-edit-dialog_code')
-      assertNonNull(editBox)
-      CurrentState.setTexItemCode(targetItemId, editBox.textContent ?? '')
-
-      // タイムスタンプを更新
-      CurrentState.updateItemTimestamp(targetItemId)
-
-      // ダイアログを閉じる
-      CurrentState.setDialog(null)
-      Rerenderer.instance.rerender()
-    },
+    onClickFinishButton,
     onClickCancelButton: () => {
       // ダイアログを閉じる
       CurrentState.setDialog(null)
       Rerenderer.instance.rerender()
+    },
+    onKeyDown: (event) => {
+      switch (InputId.fromKeyboardEvent(event)) {
+        case '1000Enter':
+          onClickFinishButton()
+          break
+      }
     },
   }
 }
