bcc67d5083c2b07050047dcfaaf57e5a4647a548
ootidea <ootidea@gmail.com>
2021-07-03 15:32:49 +0900

    TeX編集ダイアログ完了時、コードが空ならそのアイテムを削除するよう修正

--- src/TreeifyTab/View/Dialog/TexEditDialogProps.ts
+++ src/TreeifyTab/View/Dialog/TexEditDialogProps.ts
@@ -3,6 +3,7 @@ import {CurrentState} from 'src/TreeifyTab/Internal/CurrentState'
 import {InputId} from 'src/TreeifyTab/Internal/InputId'
 import {Internal} from 'src/TreeifyTab/Internal/Internal'
 import {ItemPath} from 'src/TreeifyTab/Internal/ItemPath'
+import {NullaryCommand} from 'src/TreeifyTab/Internal/NullaryCommand'
 import {TexEditDialog} from 'src/TreeifyTab/Internal/State'
 import {Rerenderer} from 'src/TreeifyTab/Rerenderer'
 
@@ -18,13 +19,23 @@ export function createTexEditDialogProps(dialog: TexEditDialog): TexEditDialogPr
   const onClickFinishButton = () => {
     const targetItemId = ItemPath.getItemId(targetItemPath)
 
-    // コードを更新
     const editBox = document.querySelector<HTMLDivElement>('.tex-edit-dialog_code')
     assertNonNull(editBox)
-    CurrentState.setTexItemCode(targetItemId, editBox.textContent ?? '')
+    const code = editBox.textContent ?? ''
 
-    // タイムスタンプを更新
-    CurrentState.updateItemTimestamp(targetItemId)
+    if (code.trim() !== '') {
+      // コードが空でない場合
+
+      // コードを更新
+      CurrentState.setTexItemCode(targetItemId, code)
+      // タイムスタンプを更新
+      CurrentState.updateItemTimestamp(targetItemId)
+    } else {
+      // コードが空の場合
+
+      // TeXアイテムを削除
+      NullaryCommand.removeEdge()
+    }
 
     // ダイアログを閉じる
     CurrentState.setDialog(null)
