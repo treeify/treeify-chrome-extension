cfbe85a383386de711c06e76104b240f7228105a
ootidea <ootidea@gmail.com>
2021-07-05 14:04:28 +0900

    ページツリーのバレット表示を一部実装

--- src/TreeifyTab/View/LeftSidebar/PageTreeBulletAndIndent.svelte
+++ src/TreeifyTab/View/LeftSidebar/PageTreeBulletAndIndent.svelte
@@ -27,8 +27,16 @@
     /* インデントラインの色（ホバー時） */
     --page-tree-indent-line-hover-color: hsl(0, 0%, 70%);
 
-    /* バレットのサイズ（正方形の一辺の長さ） */
-    --page-tree-bullet-size: 0.7em;
+    /* バレット領域のサイズ（正方形の一辺の長さ） */
+    --page-tree-bullet-area-size: 0.7em;
+
+    /* 子を持たないノードのバレット（円形）の直径 */
+    --page-tree-bullet-dot-diameter: 0.25em;
+
+    /* 子を持つノードのバレット（二等辺三角形▼）の幅 */
+    --page-tree-bullet-triangle-width: 0.5em;
+    /* 子を持つノードのバレット（二等辺三角形▼）の高さ */
+    --page-tree-bullet-triangle-height: 0.3em;
   }
 
   .page-tree-bullet-and-indent {
@@ -39,7 +47,7 @@
     バレットが収まるギリギリまで幅を狭くする（メインエリアとの違いに注意）。
     ページツリーは横幅がシビアなのでインデント領域の幅を切り詰めないと厳しい。
     */
-    width: var(--page-tree-bullet-size);
+    width: var(--page-tree-bullet-area-size);
     height: 100%;
   }
 
@@ -71,15 +79,26 @@
     left: 50%;
     transform: translate(-50%, -50%);
 
-    width: var(--page-tree-bullet-size);
-    height: var(--page-tree-bullet-size);
+    width: var(--page-tree-bullet-area-size);
+    height: var(--page-tree-bullet-area-size);
   }
 
   /* 展開済み状態のバレット */
   .page-tree-bullet-and-indent_bullet-area.expanded {
-    /* TODO: 画像待ち */
-    /* アイコンを単なるマスク画像として扱い、任意の色で塗るテクニック */
-    /*background-color: hsl(0, 0%, 60%);*/
-    /*-webkit-mask-image: url('menu-down.svg');*/
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: var(--page-tree-bullet-triangle-height)
+      calc(var(--page-tree-bullet-triangle-width) / 2) 0
+      calc(var(--page-tree-bullet-triangle-width) / 2);
+    border-color: hsl(0, 0%, 50%) transparent transparent transparent;
+  }
+
+  /* 子を持たないノードのバレット */
+  .page-tree-bullet-and-indent_bullet-area.no-children {
+    width: var(--page-tree-bullet-dot-diameter);
+    height: var(--page-tree-bullet-dot-diameter);
+    border-radius: 50%;
+    background: hsl(0, 0%, 50%);
   }
 </style>
