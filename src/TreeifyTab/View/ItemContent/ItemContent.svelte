54c7a8dd385c97797f9650f0878a9c32300b262a
ootidea <ootidea@gmail.com>
2021-07-08 09:06:39 +0900

    重複したCSSを削除

--- src/TreeifyTab/View/ItemContent/ItemContent.svelte
+++ src/TreeifyTab/View/ItemContent/ItemContent.svelte
@@ -21,31 +21,3 @@
 {:else if props.itemType === ItemType.TEX}
   <TexItemContent {props} />
 {/if}
-
-<style>
-  :root {
-    /* 左サイドバーの背景色 */
-    --left-sidebar-background-color: hsl(0, 0%, 98%);
-
-    /* 左サイドバーの幅 */
-    --left-sidebar-width: 200px;
-  }
-
-  .left-sidebar {
-    width: var(--left-sidebar-width);
-    height: 100%;
-
-    overflow-y: auto;
-
-    background: var(--left-sidebar-background-color);
-    /* Dynalistを参考にしながら調整した影 */
-    box-shadow: 1.5px 0 3px hsl(0, 0%, 85%);
-  }
-
-  /* フローティング型の左サイドバー */
-  .left-sidebar.floating {
-    position: fixed;
-    /* TODO: この安易なz-index指定は必ずやトラブルの原因になるであろう */
-    z-index: 1;
-  }
-</style>
