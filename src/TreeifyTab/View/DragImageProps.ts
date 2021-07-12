823c8c50652bc25c772377423fb4296d4cb75fc9
ootidea <ootidea@gmail.com>
2021-07-12 13:50:44 +0900

    onDropコールバックの定義場所を少し移動

--- src/TreeifyTab/View/DragImageProps.ts
+++ src/TreeifyTab/View/DragImageProps.ts
@@ -18,128 +18,128 @@ export function createDragImageProps(): DragImageProps | undefined {
   if (currentDragData?.type !== 'ItemDragData') return undefined
   if (External.instance.mousePosition === undefined) return undefined
 
-  function onDrop(event: MouseEvent, draggedItemPath: ItemPath) {
-    doWithErrorCapture(() => {
-      // エッジの付け替えを行うので、エッジが定義されない場合は何もしない
-      const parentItemId = ItemPath.getParentItemId(draggedItemPath)
-      if (parentItemId === undefined) return
-
-      const itemElement = searchElementByYCoordinate(event.clientY)
-      if (itemElement === undefined) return
-
-      const itemPath: ItemPath = List(JSON.parse(itemElement.dataset.itemPath!))
-
-      const rect = itemElement.getBoundingClientRect()
-      if (event.clientX < rect.x) {
-        // Spoolへのドロップの場合
-
-        // どのアイテムのSpoolにドロップしたかを探索する
-        const spoolDroppedItemPath = searchElementByXCoordinate(itemPath, event.clientX)
-
-        const spoolDroppedItemId = ItemPath.getItemId(spoolDroppedItemPath)
-        const draggedItemId = ItemPath.getItemId(draggedItemPath)
-        const isDisplayingChildItemIds =
-          !CurrentState.getDisplayingChildItemIds(spoolDroppedItemPath).isEmpty()
-
-        if (is(spoolDroppedItemPath.take(draggedItemPath.size), draggedItemPath)) {
-          // 少し分かりづらいが、上記条件を満たすときはドラッグアンドドロップ移動を認めてはならない。
-          // 下記の2パターンが該当する。
-          // (A) 自分自身へドロップした場合（無意味だしエッジ付け替えの都合で消えてしまうので何もしなくていい）
-          // (B) 自分の子孫へドロップした場合（変な循環参照を作る危険な操作なので認めてはならない）
-          return
-        }
-
-        // エッジの付け替えを行うので、エッジが定義されない場合は何もしない
-        const parentItemId = ItemPath.getParentItemId(draggedItemPath)
-        if (parentItemId === undefined) return
-
-        if (event.altKey) {
-          if (!CurrentState.isSibling(spoolDroppedItemPath, draggedItemPath)) {
-            // エッジを追加する（トランスクルード）
-            if (isDisplayingChildItemIds) {
-              CurrentState.insertLastChildItem(spoolDroppedItemId, draggedItemId)
-            } else {
-              CurrentState.insertFirstChildItem(spoolDroppedItemId, draggedItemId)
-            }
-          }
-        } else {
-          // targetItemPathが実在しなくなるので退避
-          const aboveItemPath = CurrentState.findAboveItemPath(draggedItemPath)
-          assertNonUndefined(aboveItemPath)
-          CurrentState.setTargetItemPath(aboveItemPath)
-
-          // エッジを付け替える
-          const edge = CurrentState.removeItemGraphEdge(parentItemId, draggedItemId)
-          if (isDisplayingChildItemIds) {
-            CurrentState.insertLastChildItem(spoolDroppedItemId, draggedItemId, edge)
-          } else {
-            CurrentState.insertFirstChildItem(spoolDroppedItemId, draggedItemId, edge)
-          }
-        }
-
-        CurrentState.updateItemTimestamp(draggedItemId)
-        Rerenderer.instance.rerender()
-      } else {
-        // Spool以外の場所へのドロップの場合
-
-        if (is(itemPath.take(draggedItemPath.size), draggedItemPath)) {
-          // 少し分かりづらいが、上記条件を満たすときはドラッグアンドドロップ移動を認めてはならない。
-          // 下記の2パターンが該当する。
-          // (A) 自分自身へドロップした場合（無意味だしエッジ付け替えの都合で消えてしまうので何もしなくていい）
-          // (B) 自分の子孫へドロップした場合（変な循環参照を作る危険な操作なので認めてはならない）
-          return
-        }
-
-        const draggedItemId = ItemPath.getItemId(draggedItemPath)
-
-        // ドロップ先要素の上端を0%、下端を100%として、マウスが何%にいるのかを計算する（0~1で表現）
-        const ratio = (event.clientY - rect.top) / (rect.bottom - rect.top)
-        if (ratio <= 0.5) {
-          // ドロップ先座標がドロップ先要素の上の方の場合
-
-          // ドロップ先がアクティブページなら何もしない
-          if (!ItemPath.hasParent(itemPath)) return
-
-          if (event.altKey) {
-            if (!CurrentState.isSibling(itemPath, draggedItemPath)) {
-              // エッジを追加する（トランスクルード）
-              const newItemPath = CurrentState.insertPrevSiblingItem(itemPath, draggedItemId)
-              CurrentState.setTargetItemPath(newItemPath)
-            }
-          } else {
-            // エッジを付け替える
-            const edge = CurrentState.removeItemGraphEdge(parentItemId, draggedItemId)
-            const newItemPath = CurrentState.insertPrevSiblingItem(itemPath, draggedItemId, edge)
-            CurrentState.setTargetItemPath(newItemPath)
-          }
-        } else {
-          // ドロップ先座標がドロップ先要素の下の方の場合
-
-          if (event.altKey) {
-            if (!CurrentState.isSibling(itemPath, draggedItemPath)) {
-              // エッジを追加する（トランスクルード）
-              const newItemPath = CurrentState.insertBelowItem(itemPath, draggedItemId)
-              CurrentState.setTargetItemPath(newItemPath)
-            }
-          } else {
-            // エッジを付け替える
-            const edge = CurrentState.removeItemGraphEdge(parentItemId, draggedItemId)
-            const newItemPath = CurrentState.insertBelowItem(itemPath, draggedItemId, edge)
-            CurrentState.setTargetItemPath(newItemPath)
-          }
-        }
-
-        CurrentState.updateItemTimestamp(draggedItemId)
-        Rerenderer.instance.rerender()
-      }
-    })
-  }
-
   return {
     mousePosition: External.instance.mousePosition,
     itemPath: currentDragData.itemPath,
     onDrop,
   }
+}
+
+function onDrop(event: MouseEvent, draggedItemPath: ItemPath) {
+  doWithErrorCapture(() => {
+    // エッジの付け替えを行うので、エッジが定義されない場合は何もしない
+    const parentItemId = ItemPath.getParentItemId(draggedItemPath)
+    if (parentItemId === undefined) return
+
+    const itemElement = searchElementByYCoordinate(event.clientY)
+    if (itemElement === undefined) return
+
+    const itemPath: ItemPath = List(JSON.parse(itemElement.dataset.itemPath!))
+
+    const rect = itemElement.getBoundingClientRect()
+    if (event.clientX < rect.x) {
+      // Spoolへのドロップの場合
+
+      // どのアイテムのSpoolにドロップしたかを探索する
+      const spoolDroppedItemPath = searchElementByXCoordinate(itemPath, event.clientX)
+
+      const spoolDroppedItemId = ItemPath.getItemId(spoolDroppedItemPath)
+      const draggedItemId = ItemPath.getItemId(draggedItemPath)
+      const isDisplayingChildItemIds =
+        !CurrentState.getDisplayingChildItemIds(spoolDroppedItemPath).isEmpty()
+
+      if (is(spoolDroppedItemPath.take(draggedItemPath.size), draggedItemPath)) {
+        // 少し分かりづらいが、上記条件を満たすときはドラッグアンドドロップ移動を認めてはならない。
+        // 下記の2パターンが該当する。
+        // (A) 自分自身へドロップした場合（無意味だしエッジ付け替えの都合で消えてしまうので何もしなくていい）
+        // (B) 自分の子孫へドロップした場合（変な循環参照を作る危険な操作なので認めてはならない）
+        return
+      }
+
+      // エッジの付け替えを行うので、エッジが定義されない場合は何もしない
+      const parentItemId = ItemPath.getParentItemId(draggedItemPath)
+      if (parentItemId === undefined) return
+
+      if (event.altKey) {
+        if (!CurrentState.isSibling(spoolDroppedItemPath, draggedItemPath)) {
+          // エッジを追加する（トランスクルード）
+          if (isDisplayingChildItemIds) {
+            CurrentState.insertLastChildItem(spoolDroppedItemId, draggedItemId)
+          } else {
+            CurrentState.insertFirstChildItem(spoolDroppedItemId, draggedItemId)
+          }
+        }
+      } else {
+        // targetItemPathが実在しなくなるので退避
+        const aboveItemPath = CurrentState.findAboveItemPath(draggedItemPath)
+        assertNonUndefined(aboveItemPath)
+        CurrentState.setTargetItemPath(aboveItemPath)
+
+        // エッジを付け替える
+        const edge = CurrentState.removeItemGraphEdge(parentItemId, draggedItemId)
+        if (isDisplayingChildItemIds) {
+          CurrentState.insertLastChildItem(spoolDroppedItemId, draggedItemId, edge)
+        } else {
+          CurrentState.insertFirstChildItem(spoolDroppedItemId, draggedItemId, edge)
+        }
+      }
+
+      CurrentState.updateItemTimestamp(draggedItemId)
+      Rerenderer.instance.rerender()
+    } else {
+      // Spool以外の場所へのドロップの場合
+
+      if (is(itemPath.take(draggedItemPath.size), draggedItemPath)) {
+        // 少し分かりづらいが、上記条件を満たすときはドラッグアンドドロップ移動を認めてはならない。
+        // 下記の2パターンが該当する。
+        // (A) 自分自身へドロップした場合（無意味だしエッジ付け替えの都合で消えてしまうので何もしなくていい）
+        // (B) 自分の子孫へドロップした場合（変な循環参照を作る危険な操作なので認めてはならない）
+        return
+      }
+
+      const draggedItemId = ItemPath.getItemId(draggedItemPath)
+
+      // ドロップ先要素の上端を0%、下端を100%として、マウスが何%にいるのかを計算する（0~1で表現）
+      const ratio = (event.clientY - rect.top) / (rect.bottom - rect.top)
+      if (ratio <= 0.5) {
+        // ドロップ先座標がドロップ先要素の上の方の場合
+
+        // ドロップ先がアクティブページなら何もしない
+        if (!ItemPath.hasParent(itemPath)) return
+
+        if (event.altKey) {
+          if (!CurrentState.isSibling(itemPath, draggedItemPath)) {
+            // エッジを追加する（トランスクルード）
+            const newItemPath = CurrentState.insertPrevSiblingItem(itemPath, draggedItemId)
+            CurrentState.setTargetItemPath(newItemPath)
+          }
+        } else {
+          // エッジを付け替える
+          const edge = CurrentState.removeItemGraphEdge(parentItemId, draggedItemId)
+          const newItemPath = CurrentState.insertPrevSiblingItem(itemPath, draggedItemId, edge)
+          CurrentState.setTargetItemPath(newItemPath)
+        }
+      } else {
+        // ドロップ先座標がドロップ先要素の下の方の場合
+
+        if (event.altKey) {
+          if (!CurrentState.isSibling(itemPath, draggedItemPath)) {
+            // エッジを追加する（トランスクルード）
+            const newItemPath = CurrentState.insertBelowItem(itemPath, draggedItemId)
+            CurrentState.setTargetItemPath(newItemPath)
+          }
+        } else {
+          // エッジを付け替える
+          const edge = CurrentState.removeItemGraphEdge(parentItemId, draggedItemId)
+          const newItemPath = CurrentState.insertBelowItem(itemPath, draggedItemId, edge)
+          CurrentState.setTargetItemPath(newItemPath)
+        }
+      }
+
+      CurrentState.updateItemTimestamp(draggedItemId)
+      Rerenderer.instance.rerender()
+    }
+  })
 }
 
 // 指定されたY座標に表示されているアイテムのコンテンツエリアのDOM要素を返す
