aea02e745271b1ecd2aac280542833e6f74dc71b
ootidea <ootidea@gmail.com>
2021-07-15 15:19:55 +0900

    検索結果にページのコンテンツコンポーネントを表示する機能を復活

--- src/TreeifyTab/View/Dialog/SearchResultPage.svelte
+++ src/TreeifyTab/View/Dialog/SearchResultPage.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import ItemContent from '../ItemContent/ItemContent.svelte'
   import SearchResultItem from './SearchResultItem.svelte'
   import {SearchResultPageProps} from './SearchResultPageProps'
 
@@ -6,13 +7,16 @@
 </script>
 
 <div class="search-result-page">
-  {#each props.searchResultItemPropses.toArray() as searchResultItemProps (searchResultItemProps.itemPath.toString())}
-    <SearchResultItem props={searchResultItemProps} />
-  {/each}
+  <ItemContent props={props.pageContent} />
+  <div class="search-result-page_tree">
+    {#each props.searchResultItemPropses.toArray() as searchResultItemProps (searchResultItemProps.itemPath.toString())}
+      <SearchResultItem props={searchResultItemProps} />
+    {/each}
+  </div>
 </div>
 
 <style>
-  .search-result-page {
+  .search-result-page_tree {
     border: solid 1px hsl(0, 0%, 70%);
     border-radius: 0.7em;
     padding: 0.5em 0.5em 0.5em 1em;
--- src/TreeifyTab/View/Dialog/SearchResultPageProps.ts
+++ src/TreeifyTab/View/Dialog/SearchResultPageProps.ts
@@ -4,13 +4,19 @@ import {
   createSearchResultItemPropses,
   SearchResultItemProps,
 } from 'src/TreeifyTab/View/Dialog/SearchResultItemProps'
+import {
+  createItemContentProps,
+  ItemContentProps,
+} from 'src/TreeifyTab/View/ItemContent/ItemContentProps'
 
 export type SearchResultPageProps = {
+  pageContent: ItemContentProps
   searchResultItemPropses: List<SearchResultItemProps>
 }
 
 export function createSearchResultPageProps(itemPaths: List<ItemPath>) {
   return {
+    pageContent: createItemContentProps(ItemPath.getRootItemId(itemPaths.first())),
     searchResultItemPropses: createSearchResultItemPropses(itemPaths),
   }
 }
