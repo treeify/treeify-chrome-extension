011bcda583cdfde0a635d2ef558e0f149eee40f7
ootidea <ootidea@gmail.com>
2021-07-18 12:58:51 +0900

    OPMLファイルエクスポートボタンをツールバーから環境設定ダイアログに移動

--- src/TreeifyTab/View/Dialog/PreferenceDialog.svelte
+++ src/TreeifyTab/View/Dialog/PreferenceDialog.svelte
@@ -1,8 +1,31 @@
 <script lang="ts">
+  import {List} from 'immutable'
+  import {assert} from '../../../Common/Debug/assert'
+  import {TOP_ITEM_ID} from '../../basicType'
+  import {doWithErrorCapture} from '../../errorCapture'
+  import {toOpmlString} from '../../Internal/ImportExport/opml'
+  import {Internal} from '../../Internal/Internal'
+  import {State} from '../../Internal/State'
   import CommonDialog from './CommonDialog.svelte'
   import {PreferenceDialogProps} from './PreferenceDialogProps'
 
   export let props: PreferenceDialogProps
+
+  function onClickExportButton() {
+    doWithErrorCapture(() => {
+      const fileName = 'treeify.opml'
+
+      const content = toOpmlString(List.of(List.of(TOP_ITEM_ID)))
+      const aElement = document.createElement('a')
+      aElement.href = window.URL.createObjectURL(new Blob([content], {type: 'application/xml'}))
+      aElement.download = fileName
+      aElement.click()
+
+      assert(State.isValid(Internal.instance.state))
+    })
+  }
 </script>
 
-<CommonDialog title="環境設定" />
+<CommonDialog title="環境設定">
+  <button on:click={onClickExportButton}>OPMLファイルをエクスポート</button>
+</CommonDialog>
--- src/TreeifyTab/View/Toolbar/Toolbar.svelte
+++ src/TreeifyTab/View/Toolbar/Toolbar.svelte
@@ -1,11 +1,4 @@
 <script lang="ts">
-  import {List} from 'immutable'
-  import {assert} from '../../../Common/Debug/assert'
-  import {TOP_ITEM_ID} from '../../basicType'
-  import {doWithErrorCapture} from '../../errorCapture'
-  import {toOpmlString} from '../../Internal/ImportExport/opml'
-  import {Internal} from '../../Internal/Internal'
-  import {State} from '../../Internal/State'
   import CodeBlockItemCreationButton from './CodeBlockItemCreationButton.svelte'
   import DataFolderPickerOpenButton from './DataFolderPickerOpenButton.svelte'
   import PreferenceButton from './PreferenceButton.svelte'
@@ -13,25 +6,9 @@
   import {ToolbarProps} from './ToolbarProps'
 
   export let props: ToolbarProps
-
-  function onClickExportButton() {
-    doWithErrorCapture(() => {
-      const fileName = 'treeify.opml'
-
-      const content = toOpmlString(List.of(List.of(TOP_ITEM_ID)))
-      const aElement = document.createElement('a')
-      aElement.href = window.URL.createObjectURL(new Blob([content], {type: 'application/xml'}))
-      aElement.download = fileName
-      aElement.click()
-
-      assert(State.isValid(Internal.instance.state))
-    })
-  }
 </script>
 
 <div class="toolbar">
-  <!-- TODO: このボタンはここではなく設定画面の中にあるべき -->
-  <button on:click={onClickExportButton}>OPMLファイルをエクスポート</button>
   <CodeBlockItemCreationButton />
   <TexItemCreationButton />
   <DataFolderPickerOpenButton props={props.dataFolderPickerOpenButtonProps} />
