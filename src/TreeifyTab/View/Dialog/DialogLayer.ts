de20599d85dfe37954f4d08a048c884b3403fcfc
ootidea <ootidea@gmail.com>
2021-07-20 17:00:38 +0900

    不要な引数を削除

--- src/TreeifyTab/View/Dialog/DialogLayer.ts
+++ src/TreeifyTab/View/Dialog/DialogLayer.ts
@@ -1,12 +1,11 @@
 import {Dialog} from 'src/TreeifyTab/External/DialogState'
 import {External} from 'src/TreeifyTab/External/External'
-import {State} from 'src/TreeifyTab/Internal/State'
 
 export type DialogLayerProps = {
   dialog: Dialog | undefined
 }
 
-export function createDialogLayerProps(state: State): DialogLayerProps {
+export function createDialogLayerProps(): DialogLayerProps {
   return {
     dialog: External.instance.dialogState ?? undefined,
   }
