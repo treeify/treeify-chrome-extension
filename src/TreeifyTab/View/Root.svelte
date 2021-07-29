4e2066bf25d684b2c11e207963b41a2fd907d955
ootidea <ootidea@gmail.com>
2021-07-29 09:02:45 +0900

    index.cssを削除し、内容をRootコンポーネントに移動

--- src/TreeifyTab/View/Root.svelte
+++ src/TreeifyTab/View/Root.svelte
@@ -38,6 +38,29 @@
 </div>
 
 <style global lang="scss">
+  * {
+    box-sizing: border-box;
+  }
+
+  html {
+    height: 100%;
+    font-size: 15px;
+  }
+
+  body {
+    height: 100%;
+    margin: 0;
+    font-size: inherit;
+  }
+
+  img {
+    vertical-align: top;
+  }
+
+  .spa-root {
+    height: 100%;
+  }
+
   .root {
     height: 100%;
     /* ダイアログなどを他の表示物に重ねて表示するための設定 */
@@ -59,5 +82,57 @@
   .sidebar-layout {
     display: grid;
     grid-template-columns: auto minmax(0, 1fr);
+  }
+
+  .default-favicon {
+    /* lch(40.0%, 0.0, 0.0)相当 */
+    background: #5e5e5e;
+    -webkit-mask-image: url('./default-favicon.svg');
+  }
+
+  /*
+  アイコンのみ（テキストなし）の円形ボタンの共通クラス。
+  マウスホバー時の背景色変化と押下時の疑似リップルエフェクトが実装されている。
+  */
+  .icon-button {
+    border-radius: 50%;
+
+    cursor: pointer;
+
+    /* アイコンと疑似リップルエフェクトを中央寄せにする */
+    position: relative;
+  }
+  .icon-button:hover {
+    /* lch(90.0%, 0.0, 0.0)相当 */
+    background: #e2e2e2;
+  }
+  /* 疑似リップルエフェクトの終了状態 */
+  .icon-button::after {
+    content: '';
+
+    /* 中央寄せ */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: opacity 0.5s, width 0.5s, height 0.5s;
+
+    border-radius: 50%;
+
+    /* lch(50.0%, 0.0, 0.0)相当 */
+    background: #777777;
+
+    pointer-events: none;
+  }
+  /* 疑似リップルエフェクトの開始状態 */
+  .icon-button:active::after {
+    width: 0;
+    height: 0;
+    opacity: 0.5;
+    transition: opacity 0s, width 0s, height 0s;
   }
 </style>
