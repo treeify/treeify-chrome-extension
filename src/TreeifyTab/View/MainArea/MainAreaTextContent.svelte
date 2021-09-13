73fb598646add5a95a58c62b01195b27db556253
ootidea <ootidea@gmail.com>
2021-09-13 11:08:09 +0900

    出典付きテキスト項目に表示される左端の棒の縦範囲を修正

--- src/TreeifyTab/View/MainArea/MainAreaTextContent.svelte
+++ src/TreeifyTab/View/MainArea/MainAreaTextContent.svelte
@@ -25,16 +25,16 @@
 </div>
 
 <style global lang="scss">
-  .citation .main-area-text-content {
-    // lch(80.0%, 0.0, 0.0)相当
-    border-left: 0.25em solid #c6c6c6;
-    padding-left: 0.2em;
-  }
-
   // テキスト項目のcontenteditableな要素
   .main-area-text-content_content-editable {
     // contenteditableな要素のフォーカス時の枠線を非表示
     outline: none;
+
+    .citation & {
+      // lch(80.0%, 0.0, 0.0)相当
+      border-left: 0.25em solid #c6c6c6;
+      padding-left: 0.2em;
+    }
 
     // グレーアウト状態のテキスト項目
     .grayed-out &,
