b0cd30700843a9deac9151d8951513521eb88b5b
ootidea <ootidea@gmail.com>
2021-09-15 11:48:58 +0900

    行間の余白が狭すぎると感じたので修正

--- src/TreeifyTab/View/MainArea/MainArea.svelte
+++ src/TreeifyTab/View/MainArea/MainArea.svelte
@@ -27,7 +27,7 @@
   :root {
     // メインエリアのテキスト全般に適用されるline-height。
     // 階層が深くなるごとにフォントサイズなどが小さくなる仕組みを実現するために比率で指定しなければならない。
-    --main-area-line-height: 1.3;
+    --main-area-line-height: 1.45;
 
     // フォントサイズをline-height（比率指定）を乗算して、行の高さを算出する
     --main-area-calculated-line-height: calc(
