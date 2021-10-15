14c27b2b9f1a64731272e4b36a85014e62e0ac7f
ootidea <ootidea@gmail.com>
2021-10-15 22:06:36 +0900

    TextItemContentコンポーネントに出典付き時の左端の縦棒を表示

--- src/TreeifyTab/View/ItemContent/TextItemContent.svelte
+++ src/TreeifyTab/View/ItemContent/TextItemContent.svelte
@@ -5,7 +5,16 @@
   export let props: TextItemContentProps
 </script>
 
-<div class="text-item-content">{@html DomishObject.toHtml(props.domishObjects)}</div>
+<div class="text-item-content" class:citation={props.hasCite}>
+  {@html DomishObject.toHtml(props.domishObjects)}
+</div>
 
 <style global lang="scss">
+  .text-item-content {
+    &.citation {
+      // lch(80.0%, 0.0, 0.0)相当
+      border-left: 0.25em solid #c6c6c6;
+      padding-left: 0.2em;
+    }
+  }
 </style>
--- src/TreeifyTab/View/ItemContent/TextItemContentProps.ts
+++ src/TreeifyTab/View/ItemContent/TextItemContentProps.ts
@@ -6,11 +6,13 @@ import {Internal} from 'src/TreeifyTab/Internal/Internal'
 export type TextItemContentProps = {
   itemType: ItemType.TEXT
   domishObjects: List<DomishObject>
+  hasCite: boolean
 }
 
 export function createTextItemContentProps(itemId: ItemId): TextItemContentProps {
   return {
     itemType: ItemType.TEXT,
     domishObjects: Internal.instance.state.textItems[itemId].domishObjects,
+    hasCite: Internal.instance.state.items[itemId].cite !== null,
   }
 }
