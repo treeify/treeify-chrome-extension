41485fcc3006a03f8acda302d118b92422692af5
ootidea <ootidea@gmail.com>
2021-11-20 18:34:19 +0900

    Dialog型定義をDiscriminatedUnionでリプレース

--- src/TreeifyTab/External/DialogState.ts
+++ src/TreeifyTab/External/DialogState.ts
@@ -1,72 +1,70 @@
 import { Coordinate } from 'src/Common/integer'
-import { ItemId } from 'src/TreeifyTab/basicType'
+import { DiscriminatedUnion, ItemId } from 'src/TreeifyTab/basicType'
 
-export type CaptionSettingDialog = { type: 'CaptionSettingDialog' }
+export type CaptionSettingDialog = {}
 
-export type CitationSettingDialog = { type: 'CitationSettingDialog' }
+export type CitationSettingDialog = {}
 
-export type CodeBlockItemEditDialog = { type: 'CodeBlockItemEditDialog' }
+export type CodeBlockItemEditDialog = {}
 
-export type CodeBlockLanguageSettingDialog = { type: 'CodeBlockLanguageSettingDialog' }
+export type CodeBlockLanguageSettingDialog = {}
 
 export type ContextMenuDialog = {
-  type: 'ContextMenuDialog'
   mousePosition: Coordinate
 }
 
-export type CustomCssDialog = { type: 'CustomCssDialog' }
+export type CustomCssDialog = {}
 
-export type ExportDialog = { type: 'ExportDialog' }
+export type ExportDialog = {}
 
-export type ImageItemEditDialog = { type: 'ImageItemEditDialog' }
+export type ImageItemEditDialog = {}
 
-export type ItemAdditionDropdownMenuDialog = { type: 'ItemAdditionDropdownMenuDialog' }
+export type ItemAdditionDropdownMenuDialog = {}
 
-export type KeyBindingDialog = { type: 'KeyBindingDialog' }
+export type KeyBindingDialog = {}
 
-export type OtherParentsDialog = { type: 'OtherParentsDialog' }
+export type OtherParentsDialog = {}
 
-export type OtherSettingsDialog = { type: 'OtherSettingsDialog' }
+export type OtherSettingsDialog = {}
 
-export type PreferenceDropdownMenuDialog = { type: 'PreferenceDropdownMenuDialog' }
+export type PreferenceDropdownMenuDialog = {}
 
-export type SearchDialog = { type: 'SearchDialog' }
+export type SearchDialog = {}
 
 export type TabsDialog = {
-  type: 'TabsDialog'
   // このダイアログは他と異なり、専用のターゲット項目IDを持つので整合性に要注意。
   // 例えばこのダイアログを開いた状態でブラウザのタブを閉じて該当項目が削除された場合、
   // このプロパティは削除済みの項目を指すことになる。
   targetItemId: ItemId
 }
 
-export type TexEditDialog = { type: 'TexEditDialog' }
+export type TexEditDialog = {}
 
 /** ウェブページ項目のタイトル設定ダイアログが固有で持つ状態の型 */
 export type WebPageItemTitleSettingDialog = {
-  type: 'WebPageItemTitleSettingDialog'
   /** 対象となる項目のDOM要素のgetBoundingClientRect()の結果 */
   targetItemRect: DOMRect
 }
 
-export type WorkspaceDialog = { type: 'WorkspaceDialog' }
+export type WorkspaceDialog = {}
 
-export type Dialog =
-  | CaptionSettingDialog
-  | CitationSettingDialog
-  | CodeBlockItemEditDialog
-  | CodeBlockLanguageSettingDialog
-  | ContextMenuDialog
-  | CustomCssDialog
-  | ExportDialog
-  | ImageItemEditDialog
-  | ItemAdditionDropdownMenuDialog
-  | KeyBindingDialog
-  | OtherParentsDialog
-  | OtherSettingsDialog
-  | PreferenceDropdownMenuDialog
-  | SearchDialog
-  | TabsDialog
-  | TexEditDialog
-  | WebPageItemTitleSettingDialog
-  | WorkspaceDialog
+export type Dialog = DiscriminatedUnion<{
+  CaptionSettingDialog: CaptionSettingDialog
+  CitationSettingDialog: CitationSettingDialog
+  CodeBlockItemEditDialog: CodeBlockItemEditDialog
+  CodeBlockLanguageSettingDialog: CodeBlockLanguageSettingDialog
+  ContextMenuDialog: ContextMenuDialog
+  CustomCssDialog: CustomCssDialog
+  ExportDialog: ExportDialog
+  ImageItemEditDialog: ImageItemEditDialog
+  ItemAdditionDropdownMenuDialog: ItemAdditionDropdownMenuDialog
+  KeyBindingDialog: KeyBindingDialog
+  OtherParentsDialog: OtherParentsDialog
+  OtherSettingsDialog: OtherSettingsDialog
+  PreferenceDropdownMenuDialog: PreferenceDropdownMenuDialog
+  SearchDialog: SearchDialog
+  TabsDialog: TabsDialog
+  TexEditDialog: TexEditDialog
+  WebPageItemTitleSettingDialog: WebPageItemTitleSettingDialog
+  WorkspaceDialog: WorkspaceDialog
+}>
