61dce5f4c866687da2361f98d3e6c813e97ff202
ootidea <ootidea@gmail.com>
2021-11-20 19:34:46 +0900

    DiscriminatedUnionのコメントを改善

--- src/TreeifyTab/basicType.ts
+++ src/TreeifyTab/basicType.ts
@@ -39,8 +39,12 @@ export type WorkspaceId = Timestamp
 export type CommandId = string
 
 /**
- * タグ付きUnion型をボイラープレート的なコードなしで定義するためのユーティリティ。
- * この型関数では再帰的な構造を持つ代数的データ型は定義できない。
+ * タグ付きUnion型を定義するためのユーティリティ。
+ * 再帰的な構造を持つ代数的データ型は定義できない。
+ * @example
+ * DiscriminatedUnion<{ Rect: { width: number; height: number }; Circle: { radius: number } }>
+ * ↑と↓は等価
+ * { type: 'Rect'; width: number; height: number } | { type: 'Circle'; radius: number }
  */
 export type DiscriminatedUnion<T, K extends keyof T = keyof T> = K extends K
   ? { type: K } & T[K]
