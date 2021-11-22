0801bb8ce8456a8b2be55d5747ffe25adb5da49d
ootidea <ootidea@gmail.com>
2021-11-23 05:09:49 +0900

    その他の設定ダイアログにOKボタンを追加

--- src/TreeifyTab/View/Dialog/OtherSettingsDialog.svelte
+++ src/TreeifyTab/View/Dialog/OtherSettingsDialog.svelte
@@ -1,8 +1,15 @@
 <script lang="ts">
+  import { External } from 'src/TreeifyTab/External/External'
+  import { Rerenderer } from 'src/TreeifyTab/Rerenderer'
   import CommonDialog from 'src/TreeifyTab/View/Dialog/CommonDialog.svelte'
   import { OtherSettingsDialogProps } from 'src/TreeifyTab/View/Dialog/OtherSettingsDialogProps'
 
   export let props: OtherSettingsDialogProps
+
+  function closeDialog() {
+    External.instance.dialogState = undefined
+    Rerenderer.instance.rerender()
+  }
 </script>
 
 <CommonDialog title="その他の設定" showCloseButton>
@@ -23,6 +30,9 @@
       />
       マウスを画面右端まで動かすとタブを閉じてTreeifyタブを表示
     </label>
+    <div class="other-settings-dialog_button-area">
+      <button on:click={closeDialog}>OK</button>
+    </div>
   </div>
 </CommonDialog>
 
@@ -40,5 +50,11 @@
     display: block;
 
     cursor: pointer;
+  }
+
+  .other-settings-dialog_button-area {
+    // ボタンを右寄せにする
+    margin: 1em 0 0 auto;
+    width: max-content;
   }
 </style>
