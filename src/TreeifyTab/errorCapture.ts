8717225895884dbbabd1868016b55248f56b8d65
ootidea <ootidea@gmail.com>
2021-11-23 12:17:55 +0900

    一連のコミットによって役割のなくなったファイルを削除

--- src/TreeifyTab/errorCapture.ts
+++
@@ -1,7 +0,0 @@
-import { External } from 'src/TreeifyTab/External/External'
-import { Internal } from 'src/TreeifyTab/Internal/Internal'
-
-function dumpCurrentMemory() {
-  Internal.instance.dumpCurrentState()
-  External.instance.dumpCurrentState()
-}
