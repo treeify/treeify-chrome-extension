059effb20922f5f3ef6c9e96289ea0847913c1b7
ootidea <ootidea@gmail.com>
2021-11-26 03:46:16 +0900

    TexItemEditDialogPropsをほぼ削除

--- src/TreeifyTab/View/Dialog/TexItemEditDialog.svelte
+++ src/TreeifyTab/View/Dialog/TexItemEditDialog.svelte
@@ -1,41 +1,77 @@
 <script lang="ts">
   import katex from 'katex'
+  import { External } from 'src/TreeifyTab/External/External'
+  import { Command } from 'src/TreeifyTab/Internal/Command'
+  import { CurrentState } from 'src/TreeifyTab/Internal/CurrentState'
+  import { InputId } from 'src/TreeifyTab/Internal/InputId'
+  import { Internal } from 'src/TreeifyTab/Internal/Internal'
+  import { ItemPath } from 'src/TreeifyTab/Internal/ItemPath'
+  import { Rerenderer } from 'src/TreeifyTab/Rerenderer'
   import CommonDialog from 'src/TreeifyTab/View/Dialog/CommonDialog.svelte'
   import FinishAndCancelButtons from 'src/TreeifyTab/View/Dialog/FinishAndCancelButtons.svelte'
-  import { TexItemEditDialogProps } from 'src/TreeifyTab/View/Dialog/TexItemEditDialogProps'
 
-  export let props: TexItemEditDialogProps
+  const targetItemId = ItemPath.getItemId(CurrentState.getTargetItemPath())
+  const isEmptyTexItem = CurrentState.isEmptyTexItem(targetItemId)
+  const dialogTitle = isEmptyTexItem ? 'TeX項目作成' : 'TeX編集'
+  let code = Internal.instance.state.texItems[targetItemId].code
 
-  // リアルタイムプレビュー用の変数
-  let currentCode = props.code
+  function onClickFinishButton() {
+    if (code.trim() !== '') {
+      // コードが空でない場合
 
-  function onInput(event: Event) {
-    if (event.target instanceof HTMLElement) {
-      currentCode = event.target.textContent ?? ''
+      // コードを更新
+      CurrentState.setTexItemCode(targetItemId, code)
+      // タイムスタンプを更新
+      CurrentState.updateItemTimestamp(targetItemId)
+    } else {
+      // コードが空の場合
+
+      // TeX項目を削除
+      Command.removeEdge()
+    }
+
+    // ダイアログを閉じる
+    External.instance.dialogState = undefined
+    Rerenderer.instance.rerender()
+  }
+
+  function onClickCancelButton() {
+    // ダイアログを閉じる
+    External.instance.dialogState = undefined
+    onCloseDialog()
+    Rerenderer.instance.rerender()
+  }
+
+  function onKeyDown(event: KeyboardEvent) {
+    switch (InputId.fromKeyboardEvent(event)) {
+      case '1000Enter':
+        onClickFinishButton()
+        break
+    }
+  }
+
+  function onCloseDialog() {
+    if (CurrentState.isEmptyTexItem(ItemPath.getItemId(CurrentState.getTargetItemPath()))) {
+      Command.removeEdge()
     }
   }
 </script>
 
-<CommonDialog title={props.dialogTitle} onClose={props.onCloseDialog}>
-  <div class="tex-edit-dialog_content" on:keydown={props.onKeyDown}>
+<CommonDialog title={dialogTitle} onClose={onCloseDialog}>
+  <div class="tex-edit-dialog_content" on:keydown={onKeyDown}>
     <div class="tex-edit-dialog_scroll-area">
       <div
         class="tex-edit-dialog_code"
         contenteditable="plaintext-only"
         tabindex="0"
-        on:input={onInput}
-      >
-        {props.code}
-      </div>
+        bind:textContent={code}
+      />
       <div class="tex-edit-dialog_rendered-tex">
-        {@html katex.renderToString(currentCode, { throwOnError: false })}
+        {@html katex.renderToString(code, { throwOnError: false })}
       </div>
     </div>
     <div class="tex-edit-dialog_button-area">
-      <FinishAndCancelButtons
-        onClickFinishButton={props.onClickFinishButton}
-        onClickCancelButton={props.onClickCancelButton}
-      />
+      <FinishAndCancelButtons {onClickFinishButton} {onClickCancelButton} />
     </div>
   </div>
 </CommonDialog>
