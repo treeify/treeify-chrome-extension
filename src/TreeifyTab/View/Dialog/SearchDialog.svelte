f67f74db2f7321da043e9e0faa57725e215463a7
ootidea <ootidea@gmail.com>
2021-12-19 17:31:04 +0900

    検索結果のフィルタリング機能を実装

--- src/TreeifyTab/View/Dialog/SearchDialog.svelte
+++ src/TreeifyTab/View/Dialog/SearchDialog.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import { List } from 'immutable'
+  import { List, Set } from 'immutable'
+  import { ItemId, ItemType } from 'src/TreeifyTab/basicType'
   import { Command } from 'src/TreeifyTab/Internal/Command'
   import { CurrentState } from 'src/TreeifyTab/Internal/CurrentState'
   import { InputId } from 'src/TreeifyTab/Internal/InputId'
@@ -12,7 +13,11 @@
   import { createSearchResultPageProps } from 'src/TreeifyTab/View/Dialog/SearchResultPageProps.js'
 
   let searchQueryValue = ''
+  let hitItemIds: Set<ItemId> | undefined
   let searchResult: List<List<ItemPath>> | undefined
+  let itemTypes: ItemType[] = Object.values(ItemType as any)
+  $: searchResult = makeSearchResult(hitItemIds, Set(itemTypes))
+
   const workspaceId = CurrentState.getCurrentWorkspaceId()
   const searchHistory = Internal.instance.state.workspaces[workspaceId].searchHistory
 
@@ -75,47 +80,88 @@
         PropertyPath.of('workspaces', workspaceId, 'searchHistory')
       )
 
-      // 全文検索
-      const itemIds = Internal.instance.searchEngine.search(searchQueryValue)
+      hitItemIds = Internal.instance.searchEngine.search(searchQueryValue)
+    }
+  }
 
-      // ヒットした項目の所属ページを探索し、その経路をItemPathとして収集する
-      const allItemPaths = itemIds.flatMap((itemId) => CurrentState.yieldItemPaths(itemId))
+  function makeSearchResult(
+    hitItemIds: Set<ItemId> | undefined,
+    itemTypes: Set<ItemType>
+  ): List<List<ItemPath>> | undefined {
+    if (hitItemIds === undefined) return undefined
 
-      searchResult = allItemPaths
+    const filteredItemIds = hitItemIds.filter((itemId) =>
+      itemTypes.contains(Internal.instance.state.items[itemId].type)
+    )
+
+    // ヒットした項目の所属ページを探索し、その経路をItemPathとして収集する
+    const allItemPaths = filteredItemIds.flatMap((itemId) => CurrentState.yieldItemPaths(itemId))
+
+    return (
+      allItemPaths
         // ItemPathをページIDでグループ化する
         .groupBy((itemPath) => ItemPath.getRootItemId(itemPath))
         .toList()
         .map((itemPaths) => itemPaths.toList())
         // ヒットした項目数によってページの並びをソートする
         .sortBy((itemPaths) => -itemPaths.size)
-    }
+    )
   }
 </script>
 
 <CommonDialog class="search-dialog_root" title="検索" showCloseButton>
   <div class="search-dialog_content" on:keydown={onContentAreaKeyDown}>
-    <input
-      type="text"
-      class="search-dialog_search-query"
-      placeholder="検索ワード -除外ワード"
-      list="search-dialog_search-history-list"
-      bind:value={searchQueryValue}
-      on:keydown={onSearchQueryKeyDown}
-    />
-    {#if searchResult === undefined}
-      <datalist id="search-dialog_search-history-list">
-        {#each searchHistory.reverse().toArray() as searchQuery}
-          <option value={searchQuery} />
-        {/each}
-      </datalist>
-    {/if}
-    <div class="search-dialog_result">
+    <div class="search-dialog_search-box">
+      <input
+        type="text"
+        class="search-dialog_search-query"
+        placeholder="検索ワード -除外ワード"
+        list="search-dialog_search-history-list"
+        bind:value={searchQueryValue}
+        on:keydown={onSearchQueryKeyDown}
+      />
+      {#if searchResult === undefined}
+        <datalist id="search-dialog_search-history-list">
+          {#each searchHistory.reverse().toArray() as searchQuery}
+            <option value={searchQuery} />
+          {/each}
+        </datalist>
+      {/if}
+    </div>
+    <div class="search-dialog_result-area">
+      {#if searchResult?.size > 0}
+        <div class="search-dialog_filter-area">
+          表示する項目:
+          <label class="search-dialog_checkbox-label">
+            <input type="checkbox" bind:group={itemTypes} value={ItemType.TEXT} />
+            テキスト
+          </label>
+          <label class="search-dialog_checkbox-label">
+            <input type="checkbox" bind:group={itemTypes} value={ItemType.WEB_PAGE} />
+            ウェブページ
+          </label>
+          <label class="search-dialog_checkbox-label">
+            <input type="checkbox" bind:group={itemTypes} value={ItemType.IMAGE} />
+            画像
+          </label>
+          <label class="search-dialog_checkbox-label">
+            <input type="checkbox" bind:group={itemTypes} value={ItemType.CODE_BLOCK} />
+            コードブロック
+          </label>
+          <label class="search-dialog_checkbox-label">
+            <input type="checkbox" bind:group={itemTypes} value={ItemType.TEX} />
+            TeX
+          </label>
+        </div>
+      {/if}
       {#if searchResult !== undefined}
-        {#each searchResult.toArray() as itemPaths}
-          <SearchResultPage props={createSearchResultPageProps(itemPaths)} />
-        {:else}
-          <div>検索結果はありません</div>
-        {/each}
+        <div class="search-dialog_result">
+          {#each searchResult.toArray() as itemPaths}
+            <SearchResultPage props={createSearchResultPageProps(itemPaths)} />
+          {:else}
+            <div>検索結果はありません</div>
+          {/each}
+        </div>
       {/if}
     </div>
   </div>
@@ -132,18 +178,39 @@
     padding: 1em;
   }
 
+  .search-dialog_search-box {
+    display: grid;
+    grid-template-columns: minmax(0, 1fr) auto;
+    align-items: center;
+  }
+
   .search-dialog_search-query {
-    width: 100%;
+    padding-block: 0.5em;
+    padding-left: 0.5em;
+  }
+
+  .search-dialog_filter-area {
+    display: flex;
+    align-items: center;
+    padding: 0.5em;
+  }
+
+  .search-dialog_checkbox-label {
+    display: inline-flex;
+    align-items: center;
+
+    cursor: pointer;
+  }
+
+  .search-dialog_result-area {
+    padding-bottom: 1em;
+
+    overflow-y: auto;
   }
 
   .search-dialog_result {
-    margin-top: 1em;
-    padding-bottom: 1em;
-
     display: flex;
     flex-direction: column;
     row-gap: 1em;
-
-    overflow-y: auto;
   }
 </style>
