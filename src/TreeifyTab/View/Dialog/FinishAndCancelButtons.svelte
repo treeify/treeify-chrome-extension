9c7ea03821f63c7dc7f8a2e51643ac69c2322b8a
ootidea <ootidea@gmail.com>
2022-02-19 08:02:12 +0900

    Windows以外では完了ボタンとキャンセルボタンの並び順を逆にする

--- src/TreeifyTab/View/Dialog/FinishAndCancelButtons.svelte
+++ src/TreeifyTab/View/Dialog/FinishAndCancelButtons.svelte
@@ -5,16 +5,16 @@
 
 <div class="finish-and-cancel-buttons_root">
   <button
-    class="finish-and-cancel-buttons_finish-button"
-    on:mousedown|preventDefault={onClickFinishButton}
-  >
-    完了
-  </button>
-  <button
     class="finish-and-cancel-buttons_cancel-button"
     on:mousedown|preventDefault={onClickCancelButton}
   >
     キャンセル
+  </button>
+  <button
+    class="finish-and-cancel-buttons_finish-button"
+    on:mousedown|preventDefault={onClickFinishButton}
+  >
+    完了
   </button>
 </div>
 
@@ -22,5 +22,9 @@
   .finish-and-cancel-buttons_root {
     display: inline-flex;
     gap: 0.8em;
+
+    [data-os-name='Windows'] & {
+      flex-direction: row-reverse;
+    }
   }
 </style>
