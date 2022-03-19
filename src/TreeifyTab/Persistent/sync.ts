5ce8cf5f67141c860bae6f56df0ca14917cae2da
ootidea <ootidea@gmail.com>
2022-03-19 13:07:21 +0900

    リロード後の同期時に初回同期扱いになる不具合を修正

--- src/TreeifyTab/Persistent/sync.ts
+++ src/TreeifyTab/Persistent/sync.ts
@@ -1,10 +1,8 @@
-import { assertNonNull, assertNonUndefined } from 'src/Utility/Debug/assert'
-
 const GOOGLE_DRIVE_SYNCED_AT = 'GOOGLE_DRIVE_SYNCED_AT'
 let googleDriveSyncedAt: string | undefined
 
 export function getGoogleDriveSyncedAt(): string | undefined {
-  if (googleDriveSyncedAt !== undefined) {
+  if (googleDriveSyncedAt === undefined) {
     googleDriveSyncedAt = localStorage.getItem(GOOGLE_DRIVE_SYNCED_AT) ?? undefined
   }
 
@@ -12,13 +10,6 @@ export function getGoogleDriveSyncedAt(): string | undefined {
 }
 
 export function setGoogleDriveSyncedAt(dateString: string) {
-  // TODO: 同期時に初回同期扱いになる原因不明の不具合への対策兼調査用
-  if (dateString === undefined || dateString === null) {
-    assertNonNull(dateString)
-    assertNonUndefined(dateString)
-    return
-  }
-
   googleDriveSyncedAt = dateString
   localStorage.setItem(GOOGLE_DRIVE_SYNCED_AT, dateString)
 }
