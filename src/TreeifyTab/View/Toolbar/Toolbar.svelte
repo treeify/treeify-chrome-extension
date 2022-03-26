052ac0685708872477ced52dcd79207e893cff4c
ootidea <ootidea@gmail.com>
2022-03-26 18:08:25 +0900

    ツールバーの開発者用ボタンを削除

--- src/TreeifyTab/View/Toolbar/Toolbar.svelte
+++ src/TreeifyTab/View/Toolbar/Toolbar.svelte
@@ -1,43 +1,13 @@
 <script lang="ts">
-  import { TOP_ITEM_ID } from 'src/TreeifyTab/basicType'
-  import { External } from 'src/TreeifyTab/External/External'
-  import { toOpmlString } from 'src/TreeifyTab/Internal/ImportExport/opml'
-  import { Internal } from 'src/TreeifyTab/Internal/Internal'
-  import { State } from 'src/TreeifyTab/Internal/State'
   import ItemAdditionButton from 'src/TreeifyTab/View/Toolbar/ItemAdditionButton.svelte'
   import PreferenceButton from 'src/TreeifyTab/View/Toolbar/PreferenceButton.svelte'
   import SyncButton from 'src/TreeifyTab/View/Toolbar/SyncButton.svelte'
   import { ToolbarProps } from 'src/TreeifyTab/View/Toolbar/ToolbarProps'
-  import { assert } from 'src/Utility/Debug/assert'
 
   export let props: ToolbarProps
-
-  function onClickOpmlExport() {
-    const fileName = 'treeify.opml'
-
-    const content = toOpmlString([[TOP_ITEM_ID]])
-    const aElement = document.createElement('a')
-    aElement.href = window.URL.createObjectURL(new Blob([content], { type: 'application/xml' }))
-    aElement.download = fileName
-    aElement.click()
-
-    assert(Array.from(External.instance.urlToItemIdsForTabCreation.values()).flat().length === 0)
-    assert(External.instance.tabIdsToBeClosedForUnloading.size === 0)
-    assert(State.isValid(Internal.instance.state))
-  }
-
-  function onClickStateValidation() {
-    assert(Array.from(External.instance.urlToItemIdsForTabCreation.values()).flat().length === 0)
-    assert(External.instance.tabIdsToBeClosedForUnloading.size === 0)
-    assert(State.isValid(Internal.instance.state))
-  }
 </script>
 
 <div class="toolbar_root">
-  {#if process.env.NODE_ENV !== 'production'}
-    <button on:mousedown|preventDefault={onClickOpmlExport}>OPMLエクスポート</button>
-    <button on:mousedown|preventDefault={onClickStateValidation}>バリデーション</button>
-  {/if}
   <SyncButton props={props.syncButtonProps} />
   <ItemAdditionButton />
   <PreferenceButton />
